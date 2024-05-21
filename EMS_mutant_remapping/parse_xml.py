f3e8e4132abed469a5a99239c3a3a9f421b95e1f
skyungyong <s.kyungyong@berkeley.edu>
2024-05-20 20:38:44 -0700

    Create parse_xml.py

---
+++ EMS_mutant_remapping/parse_xml.py
@@ -0,0 +1,22 @@
+import xml.etree.ElementTree as ET
+
+def parse_sra_xml(file_path):
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+
+    biosamples = []
+    for biosample in root.findall(".//BioSample"):
+        biosample_id = biosample.find(".//Id[@db='BioSample']")
+        sample_name = biosample.find(".//Id[@db_label='Sample name']")
+        
+        if biosample_id is not None and sample_name is not None:
+            biosamples.append((biosample_id.text, sample_name.text))
+        else:
+            print(f'{biosample_id} and {sample_name} not properly paired')
+
+    return biosamples
+
+
+file_path = 'biosample_result.xml'  # Replace with the path to your XML file
+biosamples = parse_sra_xml(file_path)
+
