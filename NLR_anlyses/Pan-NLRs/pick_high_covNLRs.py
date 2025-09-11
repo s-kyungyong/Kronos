8ea832ef677987aa6dab0d7e64f9db8b817c6907
skyungyong <s.kyungyong@berkeley.edu>
2025-09-11 11:27:41 -0700

    Create pick_high_covNLRs.py

---
+++ NLR_anlyses/Pan-NLRs/pick_high_covNLRs.py
@@ -0,0 +1,19 @@
+from Bio import SeqIO
+
+input = "Putative_NLRs_in_Triticum_IsoSeq.aa.complete.fa"
+output = "Putative_NLRs_in_Triticum_IsoSeq.aa.complete.hc.fa"
+dmnd  = "Putative_NLRs_in_Triticum_IsoSeq.aa.complete.against.dmnd.out"
+
+with open(output, 'w') as o:
+    complete = {}
+
+    for line in open(dmnd, 'r'):
+        fields = line.split("\t")
+        qlen, hlen = int(fields[-2]), int(fields[-1])
+
+        if qlen > hlen * 0.9:
+            complete[fields[0]] = ''
+
+    for record in SeqIO.parse(input, 'fasta'):
+        if record.id in complete:
+            SeqIO.write(record, o, 'fasta')
